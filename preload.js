abe85c0c8af20b47e0bc2820615b28284cf67a77
Saravana <kettikco@gmail.com>
2012-12-18 19:56:56 -0800

    Create preload.js

---
+++ preload.js
@@ -0,0 +1,50 @@
+
+  (function( $ ){
+    $.fn.preloadimages = function(options) {
+      if(!this.plOptions) {
+        this.plOptions = $.extend( {
+            attrname: 'data-image',
+            batchnum: 10,
+          }, options);
+      } else if(options) {
+        this.plOptions = $.extend(this.plOptions, options);
+      }
+      // Preload the images in batches
+      this.plJob = function() {
+        this.preLoadTimer = null;
+        i = 0;
+        item = this.sourceArray.shift();
+        while(item != undefined) {
+           $('<img/>')[0].src = item;
+           i += 1;
+           if (i >= this.plOptions.batchnum) {
+              //Randomize the time between 1 to 1000ms
+              this.preLoadTimer = setTimeout($.proxy(this.plJob, this), Math.floor(Math.random()*1000+1));
+              return;
+           }
+           item = this.sourceArray.shift();
+        }
+        return;
+      };
+
+      this.plAppend = function(data) {
+        attrname = this.plOptions.attrname;
+
+        //Extract the source urls (based on the attribute name) from the new data and load them to the array.
+        //Note the : selector here is run within the context of the new data only ($(data))
+        images = $("div["+attrname+"]", $(data)).map(function(){return $(this).attr(attrname);}).get();
+        this.sourceArray = this.sourceArray.concat(images);
+        if (!this.preLoadTimer) {
+          //Randomize the time between 1 to 1000ms
+          this.preLoadTimer = setTimeout($.proxy(this.plJob, this), Math.floor(Math.random()*1000+1));
+        }
+      };
+
+      //Extract the source urls ('data-image') from the given selector into an array
+      attrname = this.plOptions.attrname;
+      this.sourceArray = this.map(function(){return $(this).attr(attrname);}).get();
+
+      //Randomize the time between 1 to 1000ms
+      this.preLoadTimer = setTimeout($.proxy(this.plJob, this), Math.floor(Math.random()*1000+1));
+    };
+  })( jQuery );
