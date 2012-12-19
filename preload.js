996f8d3faeebbecad60bcfe97ad61da9dbb1e692
Saravana <kettikco@gmail.com>
2012-12-19 11:36:13 +0530

    logic to load imgs one by one sequentially

--- preload.js
+++ preload.js
@@ -1,51 +1,50 @@
-  (function( $ ){
-    $.fn.preloadimages = function(options) {
-      if(!this.plOptions) {
-        this.plOptions = $.extend( {
-            attrname: 'data-image',
-            batchnum: 10,
-          }, options);
-      } else if(options) {
-        this.plOptions = $.extend(this.plOptions, options);
+//
+// Image Preload Plugin
+//
+(function( $ ){
+  $.fn.preloadimages = function(options) {
+    if(!this.plOptions) {
+      this.plOptions = $.extend( {
+          selector: "div[data-image]",
+          attrname: 'data-image',
+        }, options);
+    } else if(options) {
+      this.plOptions = $.extend(this.plOptions, options);
+    }
+    // Preload the images one after the other fom the queue
+    this.plJob = function() {
+      item = this.queue.shift();
+      plHandler = this;
+      if (item != undefined) {
+        $('<img />').attr('src', item).load(function(){
+            $('#preloadContainer').append( $(this));
+            plHandler.plJob();
+        });
       }
-      // Preload the images in batches
-      this.plJob = function() {
-        this.preLoadTimer = null;
-        i = 0;
-        item = this.sourceArray.shift();
-        while(item != undefined) {
-           $('<img />').attr('src', item).load(function(){
-             $('body').append( $(this).height('5px').width('5px'));
-           });
-           i += 1;
-           if (i >= this.plOptions.batchnum) {
-              //Randomize the time between 1 to 1000ms
-              this.preLoadTimer = setTimeout($.proxy(this.plJob, this), Math.floor(Math.random()*1000+100));
-              return;
-           }
-           item = this.sourceArray.shift();
-        }
-        return;
-      };
-
-      this.plAppend = function(data) {
-        attrname = this.plOptions.attrname;
-
-        //Extract the source urls (based on the attribute name) from the new data and load them to the array.
-        //Note the : selector here is run within the context of the new data only ($(data))
-        images = $("div["+attrname+"]", $(data)).map(function(){return $(this).attr(attrname);}).get();
-        this.sourceArray = this.sourceArray.concat(images);
-        if (!this.preLoadTimer) {
-          //Randomize the time between 1 to 1000ms
-          this.preLoadTimer = setTimeout($.proxy(this.plJob, this), Math.floor(Math.random()*1000+100));
-        }
-      };
-
-      //Extract the source urls ('data-image') from the given selector into an array
-      attrname = this.plOptions.attrname;
-      this.sourceArray = this.map(function(){return $(this).attr(attrname);}).get();
-
-      //Randomize the time between 1 to 1000ms
-      this.preLoadTimer = setTimeout($.proxy(this.plJob, this), Math.floor(Math.random()*1000+100));
+      return;
     };
-  })( jQuery );
+
+    this.plAppend = function(data) {
+      attrname = this.plOptions.attrname;
+
+      //Extract the source urls (based on the attribute name) from the new data and load them to the array.
+      //Note the : selector here is run within the context of the new data only ($(data))
+      oldlength = this.queue.length;
+      images = $(this.plOptions.selector, $(data)).map(function(){return $(this).attr(attrname);}).get();
+      this.queue = this.queue.concat(images);
+
+      //Spawn a new job if the old array length was zero (i.e the old job had completed)
+      if (oldlength == 0){
+        this.plJob();
+      }
+    };
+
+    //Extract the source urls ('data-image') from the given selector into an array
+    attrname = this.plOptions.attrname;
+    this.queue     = this.map(function(){return $(this).attr(attrname);}).get();
+    this.container = $('body').append("<div id='preloadContainer' style='display:none;'></div>");
+
+    //Load the first image
+    this.plJob();
+  };
+})( jQuery );
